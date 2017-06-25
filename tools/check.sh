6c68e83892837a40cb6b30fa7a96126150bedcc1
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2017-06-25 15:55:48 +0100

    parse success checker script

--- tools/check.sh
+++ tools/check.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+total=0
+succeeded=0
 for f in sg_script_search/{sg1,atl}/*; do
-	printf "\n$f\n";
-	runhaskell tools/scriptparse.hs $f | tail -n5
-	printf "\n";
+	total=$((total+1))
+	printf "$f: ";
+	result=$(timeout 3s runhaskell tools/scriptparse.hs $f | tail -n5)
+	if [[ "$result" == *"END CREDITS"*  || "$result" == *"ROLL CREDITS"* ]]; then
+		succeeded=$((succeeded+1))
+		printf "SUCCEEDED\n"
+	else
+		printf "FAILED\n"
+	fi
 done
+echo "Succeeded on $succeeded/$total"
