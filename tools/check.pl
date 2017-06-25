0d400b8a8be01fb47c1fb3677df2eb9d0fc5b1ca
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2017-06-26 00:01:45 +0100

    better checking script

---
+++ tools/check.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my @transcript_fnames = glob("sg_script_search/{sg1,atl}/*");
+my $i = 0;
+
+while (1) {
+  if ($i == scalar(@transcript_fnames)) {
+	last;
+  }
+  printf $transcript_fnames[$i] . ": ";
+  my $result = `timeout 3s runhaskell tools/scriptparse.hs $transcript_fnames[$i] | tail -n5`;
+  if ($result =~ m/(END CREDITS|ROLL CREDITS)/) {
+	printf "SUCCEEDED\n";
+	$i += 1;
+  } else {
+	printf "FAILED\nPlease correct and hit enter when done to retry.\n";
+	printf $result;
+	<STDIN>;
+  }
+}
+
+
