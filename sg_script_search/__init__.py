6b4bc6ebb6d35c7f0554b0f3d5c5594b7fef3602
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2017-06-26 16:22:11 +0100

    use new json data

--- sg_script_search/__init__.py
+++ sg_script_search/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request
 import os
+import json
 
 app = Flask(__name__)
 transcript_lines = {
+    'sg1':{},
+    'atl':{}
+}
+transcript_json = {
     'sg1':{},
     'atl':{}
 }
@@ -47,23 +52,34 @@ def cache_transcripts():
             [season, episode] = fname.split(".")
             with open(os.path.join(os.getcwd(), dire, fname), "r", encoding="latin-1") as transcript:
                 transcript_lines[dire][fname] = transcript.readlines()
-                    
+    os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "json"))
+    for dire in ["sg1", "atl"]:
+        for fname in os.listdir(dire):
+            [season, episode, _] = fname.split(".")
+            with open(os.path.join(os.getcwd(), dire, fname), "r", encoding="latin-1") as transcript:
+                transcript_json[dire]["{}.{}".format(season, episode)] = json.load(transcript)
     os.chdir(olddir)
+
+def strline(lines, i, j):
+    toprint = lines[i:j]
+    return ["{}: {}".format(c, l) for [c, l] in toprint]
 
 def do_search(query):
     results = []
     for (dire, series) in [("sg1", "SG1"), ("atl", "Atlantis")]:
-        for fname in transcript_lines[dire].keys():
+        for fname in transcript_json[dire].keys():
             [season, episode] = fname.split(".")
-            lines = transcript_lines[dire][fname]
-            for i in range(len(lines)):
-                if query in lines[i]:
-                    results.append({
-                        "episode": "Stargate {}: Season {}, Episode {}".format(series, season, episode),
-                        "context_before": "".join(lines[max(0,i-5):i]),
-                        "context_after": "".join(lines[min(i+1,len(lines)):i+6]),
-                        "match": lines[i],
-                        "url":"/transcripts/{}/{}.{}".format(dire, season, episode)
-                    })
+            for scene in transcript_json[dire][fname]:
+                lines = scene["speech"]
+                for i in range(len(lines)):
+                    if query in lines[i][1]:
+                        results.append({
+                            "episode": "Stargate {}: Season {}, Episode {}".format(series, season, episode),
+                            "context_before": strline(lines, max(0,i-2), i),
+                            "context_after": strline(lines, min(i+1,len(lines)), i+3),
+                            "match": "{}: {}".format(lines[i][0], lines[i][1]),
+                            "url":"/transcripts/{}/{}.{}".format(dire, season, episode),
+                            "place": scene["place"]
+                        })
                     
     return results
