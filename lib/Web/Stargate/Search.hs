4d8019480fb01cc2217b52b325f9b197a7200d2d
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2017-06-28 13:57:43 +0100

    failed attempt at reducing memory

--- lib/Web/Stargate/Search.hs
+++ lib/Web/Stargate/Search.hs
@@ -11,6 +11,9 @@ import qualified Data.Vector as V
 import Data.Vector ((!))
 import qualified Data.Vector.Algorithms.Merge as V
 import qualified Data.HashMap.Lazy as M
+import Control.Monad.ST
+import Data.STRef
+import Control.Monad
 
 data TextOrList = Text T.Text
                 | List [T.Text]
@@ -32,34 +35,52 @@ instance ToGVal m ResultsOrText where
 match :: T.Text -> T.Text -> Bool
 match query body = (T.null query) || ((T.toUpper query) `T.isInfixOf` (T.toUpper body))
 
+makeResult :: Int -> T.Text -> T.Text -> T.Text -> D.Scene -> M.HashMap T.Text TextOrList
+makeResult i series season epnum scene = M.fromList [ ("url", Text $ T.concat ["/transcripts/", series, "/", season, ".", epnum])
+                                                    , ("context_before", List $ strline speech (max 0 (i-2)) i)
+                                                    , ("match", Text $ head $ strline speech i (i+1))
+                                                    , ("context_after", List $ strline speech (min (i+1) (length speech)) (min (i+3) (length speech)))
+                                                    , ("episode", Text $ T.concat [T.toUpper series, " Season ", season, " Episode ", epnum])
+                                                    , ("place", Text $ D.place scene)
+                                                    ]
+    where speech = D.speech scene
+
+strline :: V.Vector (D.Character, T.Text) -> Int -> Int -> [T.Text]
+strline speech x y
+        | x < 0 || x > V.length speech = []
+        | y > V.length speech = strline speech x (V.length speech)
+        | otherwise =  V.toList $ V.map (\(c, l) -> T.concat [c, ": ", l]) (V.slice x (y-x) speech)
+
+sceneMatches :: D.Scene -> T.Text -> T.Text -> T.Text -> T.Text -> [Int]
+sceneMatches scene query place person present = if or [ not $ any (match present) (D.present scene)
+                                                      , not $ match place (D.place scene)
+                                                      ]
+                                                then []
+                                                else runST $ do
+  matches <- newSTRef []
+  forM_ [0..((V.length $ D.speech scene)-1)] $ \k ->
+      when ((match person (fst $ (D.speech scene) ! k)) && (match query $ snd $ (D.speech scene) ! k))
+           (modifySTRef matches (k:))
+  readSTRef matches
+
 search :: V.Vector (T.Text, T.Text, D.Episode) -> T.Text -> T.Text -> T.Text -> T.Text -> M.HashMap T.Text ResultsOrText
-search eps query place person present = let
-      toomany = if (length results) >= 500 then "yes" else "no"
-      results = V.foldl' epResults [] eps
-      epResults res (series, episode, ep) = let
-          newmatches = V.foldl' sceneResults [] ep
-          sceneResults ress scene = let
-              newSceneResults = if or [ not $ any (match present) (D.present scene)
-                                      , not $ match place (D.place scene)
-                                      ]
-                        then []
-                        else lineMatches (D.speech scene)
-              lineMatches speech = let
-                  resultOrEmpty i = if (match person (fst $ speech ! i)) && (match query (snd $ speech ! i))
-                        then [makeResult i]
-                        else []
-                  makeResult i = M.fromList [ ("url", Text $ T.concat ["/transcripts/", series, "/", episode])
-                                            , ("context_before", List $ V.toList $ strline (max 0 (i-2)) i)
-                                            , ("match", Text $ V.head $ strline i (i+1))
-                                            , ("context_after", List $ V.toList $ strline (min (i+1) (length speech)) (min (i+3) (length speech)))
-                                            , ("episode", Text $ T.concat [T.toUpper series, " Season ", season, " Episode ", epnum])
-                                            , ("place", Text $ D.place scene)
-                                            ]
-                  [season, epnum] = T.splitOn "." episode
-                  strline x y = V.map (\pair -> T.concat [fst pair, ": ", snd pair]) (V.slice x y speech)
-                in concat $ map resultOrEmpty [0..(V.length speech)-1]
-              in concat [newSceneResults, ress]
-          in concat [newmatches, res]
-  in M.fromList [("results", Results results)
-                ,("toomany", RText toomany)
-                ]
+search eps query place person present = runST $ do
+  results <- newSTRef []
+  found <- newSTRef 0
+  forM_ [0..((V.length eps)-1)] $ \i -> do
+      let (series, epstr, episode) = eps ! i
+          [season, epnum] = T.splitOn "." epstr
+      forM_ [0..((V.length episode)-1)] $ \j -> do
+          let matchks = sceneMatches (episode ! j) query place person present
+          case matchks of
+            [] -> return ()
+            ks -> do
+              let newresults = map (\k -> makeResult k series season epnum (episode ! j)) ks
+              modifySTRef results (newresults++)
+              modifySTRef found (+1)
+  nfound <- readSTRef found
+  let toomany = if nfound >= 500 then "yes" else "no"
+  finalres <- readSTRef results
+  return $ M.fromList [("results", Results finalres)
+                      ,("toomany", RText toomany)
+                      ]
