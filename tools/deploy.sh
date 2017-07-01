1884f5af916875dd3fe640867ca32dba3b35f696
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2017-07-01 02:22:46 +0100

    deploy script

---
+++ tools/deploy.sh
@@ -0,0 +1,7 @@
+ssh aws "sudo systemctl stop stargate-search; rm -rf stargate-search/*"
+rsync -a transcripts/ aws:~/stargate-search/transcripts/
+rsync -a templates/ aws:~/stargate-search/templates/
+stack install
+scp ~/.local/bin/stargate-search aws:~/stargate-search/
+ssh aws "sudo systemctl start stargate-search"
+
