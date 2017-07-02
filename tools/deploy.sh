6efeba6982b76585ac51637cabf690790c772408
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2017-07-02 01:37:20 +0100

    fix deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,6 +2,6 @@ ssh aws "sudo systemctl stop stargate-search; rm -rf stargate-search/*"
 rsync -a transcripts/ aws:~/stargate-search/transcripts/
 rsync -a templates/ aws:~/stargate-search/templates/
 stack install
-scp ~/.local/bin/stargate-search aws:~/stargate-search/
+scp ~/.local/bin/stargate-search-web aws:~/stargate-search/
 ssh aws "sudo systemctl start stargate-search"
 
