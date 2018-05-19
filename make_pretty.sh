259277c449cdeef857e002afe16827ce43aade72
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2018-05-20 00:45:50 +0100

    add trek pretty script

--- make_pretty.sh
+++ make_pretty.sh
@@ -6,13 +6,22 @@ if [[ -d pretty ]]; then
 fi
 
 mkdir pretty
-for s in transcripts/*; do
+for s in transcripts/{sg1,atl}; do
 	series=$(basename $s)
 	mkdir pretty/${series}
 	for e in transcripts/${series}/*; do
 		episode=$(basename $e)
 		echo "$series : $episode"
-		stargate-parse single <transcripts/${series}/${episode} >pretty/${series}/${episode}
+		stargate-parse single_gate <transcripts/${series}/${episode} >pretty/${series}/${episode}
+	done
+done
+for s in transcripts/{tos,tng,ds9,voy,ent}; do
+	series=$(basename $s)
+	mkdir pretty/${series}
+	for e in transcripts/${series}/*; do
+		episode=$(basename $e)
+		echo "$series : $episode"
+		stargate-parse single_trek <transcripts/${series}/${episode} >pretty/${series}/${episode}
 	done
 done
 
